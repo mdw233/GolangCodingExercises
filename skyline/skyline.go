06ae15c85a8f69ae6ba8e6049d3a21a4bc7fef94
Matt Withum <mdw233@gmail.com>
2014-04-04 04:49:53 -0700

    finished up the skyline - moved all logic into one file

--- skyline/skyline.go
+++ skyline/skyline.go
@@ -2,51 +2,68 @@ package main
 
 import (
 	"fmt"
-	"./common"
+	"os"
+	"bufio"
+	"strconv"
+	"strings"
 )
 
+type Outline struct {
+	L int
+	H int
+	R int
+}
 
 //problem here: https://stackoverflow.com/questions/1066234/the-skyline-problem
 //and here: http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=41
 func main() {
-
 	//get a list of points
-	var buildings = skylinecommon.GetTestData()
+	file, _ := os.Open("skylineinput.txt")
+	defer file.Close()
 
+	var buildings []Outline
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineSplit :=  strings.Split(scanner.Text(), " ")
+		x, _ := strconv.Atoi(lineSplit[0])
+		h, _ := strconv.Atoi(lineSplit[1])
+		y, _ := strconv.Atoi(lineSplit[2])
+
+		buildings = append(buildings, Outline { x, h, y })
+	}
+
+	PrintSkyline(buildings)
+}
+
+func PrintSkyline(buildings []Outline) {
+	
 	//build up an int array large enough to handle all points
-	//let's call that 40 for now
-	var earth [40]int
+	//let's call that 5000 for now
+	var earth [5000]int
 
 	//iterate over the list of outlines
 	// check the left border - is the height larger than previous heights - if so, record the new height
 	// do the same thing for each element between l and r
 	for _, o := range buildings {	
-		for j:= o.L; j <= o.R; j++ {
+		for j:= o.L; j < o.R; j++ {
 			if (earth[j] < o.H) {
 				earth[j] = o.H
 			}
 		}
 	}
 
-	//iterate over array
-	//1. start by finding the first non-zero height
+	//1. iterate over array
 	//2. when the height at an index changes, mark the difference between this and the last index change and add that to the line array
 	//3. then add the height to the line array
-	//4. unless the new height is 0, then mark that height of the in
 	var skyline []int
-	indexOfLastChange, lastHeight := 0, 0
+	lastHeight := 0
 	for i := 0; i < len(earth); i++ {
-
-		//if the height has changed at all, mark down the index and height
-		if lastHeight != earth[i] {
+		if (lastHeight != earth[i]) {
 			skyline = append(skyline, i)
 			skyline = append(skyline, earth[i])
+			lastHeight = earth[i]
 		}
-
-		lastHeight = earth[i]
 	}
-
-	//may actually be able to short cicuit all of this and just start at 3.
 
 	for i := 0; i < len(skyline); i++ {
 		fmt.Println(skyline[i])
